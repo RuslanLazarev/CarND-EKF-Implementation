fee48d7adc03037e721fea97e4cb4a02fa148b21
RuslanLazarev <ruslan.lazarev@gmail.com>
2018-08-07 00:16:52 +0200

    upd

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,6 +3,9 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+// Please note that the Eigen library does not initialize 
+// VectorXd or MatrixXd objects with zeros upon creation.
+
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
@@ -18,9 +21,14 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  x_ = F_ * x_ ;
+  x_ = F_ * x_;
   MatrixXd Ft = F_.transpose();
   P_ = F_ * P_ * Ft + Q_;
+  /**
+  TODO:
+    * predict the state
+  */
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -28,7 +36,7 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
-  VectorXd y = z - H_ * x_;
+	VectorXd y = z - H_ * x_;
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
@@ -45,31 +53,36 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
   double px = x_(0);
   double py = x_(1);
   double vx = x_(2);
   double vy = x_(3);
 
   double rho = sqrt(px*px + py*py);
-  double theta = atan2(py, px);
-  double rho_dot = (px*vx + py*vy) / rho;
-  VectorXd h = VectorXd(3);
-  h << rho, theta, rho_dot;
-  VectorXd y = z - h;
-  while ( y(1) > M_PI || y(1) < -M_PI ) {
-    if ( y(1) > M_PI ) {
-      y(1) -= M_PI;
+  double phi = atan2(py,px);
+  double rho_dot = (px*vx + py*vy)/rho;
+
+  while ( phi > M_PI || phi < -M_PI ) {
+    if ( phi > M_PI ) {
+      phi -= M_PI;
     } else {
-      y(1) += M_PI;
+      phi += M_PI;
     }
   }
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd K =  P_ * Ht * Si;
-  // New state
-  x_ = x_ + (K * y);
-  int x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+
+
+  VectorXd h = VectorXd(3);
+  h << rho, phi, rho_dot;
+  VectorXd y = z - h;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Ht * Si;
+
+  //new estimate
+	x_ = x_ + (K * y);
+	int x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
